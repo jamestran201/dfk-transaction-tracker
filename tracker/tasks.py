4bd04096937da090f009cb2f0d45e7cd1174869b
James Tran <tmnhat2001@gmail.com>
2021-12-25 15:36:35 -0500

    Set task status to failed when there is an error

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -5,7 +5,14 @@ from transaction_processor.utils.transaction_parser import TransactionParser
 
 @app.task()
 def sync_transactions(wallet_address, sync_task_id):
-    TransactionParser(wallet_address).get_transactions()
+    try:
+        TransactionParser(wallet_address).get_transactions()
+    except Exception as e:
+        sync_task = TransactionSynchronization.objects.get(id=sync_task_id)
+        sync_task.status = "FAILED"
+        sync_task.save()
+
+        raise e
 
     sync_task = TransactionSynchronization.objects.get(id=sync_task_id)
     sync_task.status = "SUCCESS"
