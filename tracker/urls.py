b5cbf1e43a5b4ca51fd5c349682dd5dc57fbc856
James Tran <tmnhat2001@gmail.com>
2021-12-29 14:52:58 -0500

    WIP: transactions page

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from tracker.views import landing_page
 from tracker.views.wallet_address import WalletAddressView
+from tracker.views.transactions import TransactionsView
 
 
 app_name = "tracker"
 urlpatterns = [
     path('', landing_page.index, name='index'),
-    path('wallet_address/', WalletAddressView.as_view(), name="wallet_address")
+    path('wallet_address/', WalletAddressView.as_view(), name="wallet_address"),
+    path('transactions/', TransactionsView.as_view(), name="transactions"),
 ]
