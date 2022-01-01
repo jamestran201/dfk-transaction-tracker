3c1d8c6acea7d70b40cecf8179dc31b8d2b3a924
James Tran <tmnhat2001@gmail.com>
2021-12-31 23:11:31 -0500

    Add instructions for building Docker images

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -2,11 +2,14 @@
 
 export DOCKER_BUILDKIT=1
 
+ACCOUNT_NAME="$1"
+BUILD_TAG="$2"
+
 # Dump requirements.txt to docker_build/ using Poetry
 poetry export --format requirements.txt --output docker_build/requirements.txt --without-hashes
 
 # Build Docker image for Celery
-docker build --tag dfk-transaction-tracker-celery-worker:v0.1.0 --target celery_build .
+docker build --tag "$ACCOUNT_NAME/dfk-transaction-tracker-celery-worker:$BUILD_TAG" --target celery_build .
 
 # Build Docker image for Django app
-docker build --tag dfk-transaction-tracker-app:v0.1.0 --target app_build .
+docker build --tag "$ACCOUNT_NAME/dfk-transaction-tracker-app:$BUILD_TAG" --target app_build .
