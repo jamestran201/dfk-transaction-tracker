a3d3d98b1d96e80db6e13c4046f4f9ea4599df15
James Tran <tmnhat2001@gmail.com>
2022-01-01 17:20:07 -0500

    Update scripts and instructions to use Google Cloud

--- scripts/push-docker-images.sh
+++ scripts/push-docker-images.sh
@@ -2,6 +2,6 @@
 
 BUILD_TAG="$1"
 
-docker push "jamestran/dfk-transaction-tracker-celery-worker:$BUILD_TAG"
+docker push "us-central1-docker.pkg.dev/integral-nimbus-336917/dfk-transaction-tracker/dfk-transaction-tracker-celery-worker:$BUILD_TAG"
 
-docker push "jamestran/dfk-transaction-tracker-app:$BUILD_TAG"
+docker push "us-central1-docker.pkg.dev/integral-nimbus-336917/dfk-transaction-tracker/dfk-transaction-tracker-app:$BUILD_TAG"
