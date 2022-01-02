957252f7daa0e3b8a0322d024bddd2cef91e4d0f
James Tran <tmnhat2001@gmail.com>
2022-01-02 15:45:47 -0500

    Add endpoint for production

--- static/js/wallet_address_post.js
+++ static/js/wallet_address_post.js
@@ -1,13 +1,15 @@
+// const endpoint = "http://localhost:8000/";
+const endpoint = "https://dfk-transaction-tracker-app-mlienhkuya-uc.a.run.app/"
 const walletAddress = document.getElementById("wallet-address").value;
 
 poll();
 
 function poll() {
-  fetch(`http://localhost:8000/wallet_address?wallet_address=${walletAddress}`)
+  fetch(`${endpoint}wallet_address?wallet_address=${walletAddress}`)
     .then(response => response.json())
     .then(json => {
       if (json["status"] === "success") {
-        window.location.replace(`http://localhost:8000/transactions?wallet_address=${walletAddress}&page=1`);
+        window.location.replace(`${endpoint}transactions?wallet_address=${walletAddress}&page=1`);
       } else if (json["status"] === "in_progress") {
         setTimeout(poll, 3000);
       }
