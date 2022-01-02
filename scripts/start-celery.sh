61e4704dfa6e2f87bf75752edf253fdc723536de
James Tran <tmnhat2001@gmail.com>
2022-01-02 00:37:55 -0500

    Hack to deploy celery worker using Cloud Run

--- scripts/start-celery.sh
+++ scripts/start-celery.sh
@@ -5,4 +5,6 @@ set -o pipefail
 set -o nounset
 
 
-exec celery -A dfk_transaction_tracker worker -l INFO
+celery -A dfk_transaction_tracker worker -l INFO --detach --concurrency 1
+
+gunicorn dfk_transaction_tracker.wsgi --bind 0.0.0.0:8000
